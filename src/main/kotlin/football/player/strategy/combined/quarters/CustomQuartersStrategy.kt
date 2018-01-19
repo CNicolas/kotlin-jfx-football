516469cf78f73a49a972deba67097ec149ba090e
Clement Nicolas <clement.nicolas-ext@amundi.com>
2018-01-19 16:31:54 +0100

    Add Random and Custom Quarters strategies

---
+++ src/main/kotlin/football/player/strategy/combined/quarters/CustomQuartersStrategy.kt
@@ -0,0 +1,11 @@
+package football.player.strategy.combined.quarters
+
+import football.player.SideInTeam
+import football.player.strategy.PlayerStrategy
+
+class CustomQuartersStrategy(override val side: SideInTeam,
+                             override val defenseStrategy: PlayerStrategy,
+                             override val midDefenseStrategy: PlayerStrategy,
+                             override val midAttackStrategy: PlayerStrategy,
+                             override val attackStrategy: PlayerStrategy) : AbstractCombinedQuartersStrategy() {
+}
---
+++ src/main/kotlin/football/player/strategy/combined/quarters/RandomQuartersStrategy.kt
@@ -0,0 +1,16 @@
+package football.player.strategy.combined.quarters
+
+import football.player.SideInTeam
+import football.player.strategy.PlayerStrategy
+import helpers.NUMBER_OF_DISTINCT_STRATEGIES
+import helpers.createStrategyByNumberAndSide
+import java.util.*
+
+class RandomQuartersStrategy(override val side: SideInTeam, random: Random) : AbstractCombinedQuartersStrategy() {
+    override val defenseStrategy: PlayerStrategy = createStrategyByNumberAndSide(random.nextInt(NUMBER_OF_DISTINCT_STRATEGIES), side)
+    override val midDefenseStrategy: PlayerStrategy = createStrategyByNumberAndSide(random.nextInt(NUMBER_OF_DISTINCT_STRATEGIES), side)
+    override val midAttackStrategy: PlayerStrategy = createStrategyByNumberAndSide(random.nextInt(NUMBER_OF_DISTINCT_STRATEGIES), side)
+    override val attackStrategy: PlayerStrategy = createStrategyByNumberAndSide(random.nextInt(NUMBER_OF_DISTINCT_STRATEGIES), side)
+
+    override fun toString(): String = "${javaClass.simpleName}($defenseStrategy, $midDefenseStrategy, $midAttackStrategy, $attackStrategy)"
+}
